058a9461798cce869615112d2b73415bd53fec15
Mehoff <mixov2000@gmail.com>
2022-02-09 20:07:44 +0200

    Generate field

--- app/src/main/java/com/step/barley_breakstep/classes/Game.java
+++ app/src/main/java/com/step/barley_breakstep/classes/Game.java
@@ -1,20 +1,25 @@
 package com.step.barley_breakstep.classes;
 
+import android.os.Debug;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
 
-    private int[][] _field = new int[4][4];
-    private TextView[][] _textViews = new TextView[4][4];
+    private final int SIZE = 4 * 4;
+    private final String TAG = "MyApp";
+    private int[] _field;
+    private TextView[] _textViews;
 
     public Game(TextView[] textViews) throws Exception {
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                _textViews[i][j] = textViews[i + j];
-            }
+        if(textViews.length != SIZE){
+            throw new Exception("Text views array length must be size of: " + SIZE + ", got: " + textViews.length);
         }
+        _textViews = textViews;
+        _field = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9, 10, 11, 12 ,13, 14, 15 };
     }
 
     public void init(){
@@ -22,29 +27,30 @@ public class Game {
     }
 
     private void setViewsTextToFieldValues(){
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                _textViews[i][j].setText(_field[i][j] + "");
+        for(int i = 0; i < SIZE; i++){
+            if(_field[i] == 0){
+                _textViews[i].setText(" ");
+            } else {
+            _textViews[i].setText(_field[i] + "");
             }
         }
     }
-
+    
     private void shuffleField(){
-        shuffle(_field);
         setViewsTextToFieldValues();
     }
 
-    void shuffle(int[][] a) {
-        Random random = new Random();
-        for (int i = a.length - 1; i > 0; i--) {
-            for (int j = a[i].length - 1; j > 0; j--) {
-                int m = random.nextInt(i + 1);
-                int n = random.nextInt(j + 1);
-
-                int temp = a[i][j];
-                a[i][j] = a[m][n];
-                a[m][n] = temp;
-            }
+    private void shuffle(int[] ar)
+    {
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
         }
     }
+
+
 }
